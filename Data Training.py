bba7220e6af25ef5e7077b4748cff9eda0beddb7
Kotagiri Yaswanth Sai <yaswanthsaikotagiri1@gmail.com>
2022-12-14 07:58:23 +0530

    Update

--- Data Training.py
+++ Data Training.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+
 
 df = pd.read_csv("Asana.csv")
 
@@ -28,6 +30,12 @@ dtc.fit(x_train, y_train)
 
 knn = KNeighborsClassifier()
 knn.fit(x_train, y_train)
+
+scores = cross_val_score(rfr, x, y, cv=26)
+
+print("Accuracy for each fold:", scores)
+
+print("Average accuracy:", scores.mean())
 
 print("Random Forest Classifier : ", round(accuracy_score(y_test,rfr.predict(x_test)), 2))
 print("Decision Tree Classifier : ", round(accuracy_score(y_test,dtc.predict(x_test)), 2))
